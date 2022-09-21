68d43b72fb73d55ebf3636d850fc002a65d99d4d
Gabriel Gonçalves <77746344+Gabrielgsoares@users.noreply.github.com>
2022-09-21 15:10:59 -0300

    Add files via upload

---
+++ Scripts/Gamer_controler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Gamer_controler : MonoBehaviour
+{
+
+    public int totalScore;
+    public Text scoreText;
+
+    public GameObject gameOver;
+
+    public static Gamer_controler instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       instance = this; 
+    }
+
+    public void UpdateScoreText(){
+        scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGamerOver(){
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName){
+        SceneManager.LoadScene(lvlName);
+    }
+}
